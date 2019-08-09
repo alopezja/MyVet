af86fa7d1d7f9bdea25fdebb0d973785974bd394
ALEX DAVID LOPEZ JARAMILLO <alexlopez39007@correo.itm.edu.co>
2019-08-09 00:23:03 -0500

    New All

---
+++ MyVet/Data/DataContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyVet.Data.Entities;
+using MyVet.Web.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyVet.Data
+{
+    public class DataContext : DbContext //Inherit
+    {
+        //Create a constructor ctor Tab(x2)
+        public DataContext(DbContextOptions<DataContext> options) : base(options) //DbContextOptions is a BD based on DataContext named options and object option go to the BD constructor
+        {
+        }
+
+        //Matriculate tables
+
+        public DbSet<Agenda> Agendas { get; set; } 
+
+        public DbSet<History> Histories { get; set; }
+
+        public DbSet<Owner> Owners { get; set; } //Propiety DbSet, Model Owner and Colection Owners
+
+        public DbSet<Pet> Pets { get; set; }
+
+        public DbSet<PetType> PetTypes { get; set; }
+
+        public DbSet<ServiceType> ServiceTypes { get; set; }
+
+        //Summary =>
+        //We have a table Entity Owner with structure BD and our BD will have that structure
+
+
+    }
+}
