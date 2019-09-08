da2f6800765f26cbfc7e4a85fad9f85ae129787b
ALEX DAVID LOPEZ JARAMILLO <alexlopez39007@correo.itm.edu.co>
2019-09-08 14:48:11 -0500

    Front Pets Sep 8

--- MyVet.Prism/MyVet.Prism/ViewModels/PetsPageViewModel.cs
+++ MyVet.Prism/MyVet.Prism/ViewModels/PetsPageViewModel.cs
@@ -1,15 +1,24 @@
 ﻿using MyVet.Common.Models;
 using Prism.Navigation;
+using System.Collections.ObjectModel;
 
 namespace MyVet.Prism.ViewModels
 {
     public class PetsPageViewModel : ViewModelBase
     {
         private OwnerResponse _owner;
+        private ObservableCollection<PetResponse> _pets;
+
         public PetsPageViewModel(
             INavigationService navigationService) : base(navigationService)
         {
             Title = "Pets";
+        }
+
+        public ObservableCollection<PetResponse> Pets
+        {
+            get => _pets;
+            set => SetProperty(ref _pets, value);
         }
 
         public override void OnNavigatingTo(INavigationParameters parameters)
@@ -19,6 +28,8 @@ namespace MyVet.Prism.ViewModels
             if (parameters.ContainsKey("owner"))
             {
                 _owner = parameters.GetValue<OwnerResponse>("owner");
+                Title = $"Pets of: { _owner.FullName}";
+                Pets = new ObservableCollection<PetResponse>(_owner.Pets);
 
             }
         }
